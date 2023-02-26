056f6056d652c7357babbd8b240db679b96ffadd
JorGBit <113969001+JorGBit@users.noreply.github.com>
2023-02-26 16:28:35 +0100

    FRONTEND

---
+++ notes-frontend/src/components/NewNote.js
@@ -0,0 +1,5 @@
+/** @format */
+
+export const NewNote = () => {
+  return <form>FORMULARIO</form>;
+};
---
+++ notes-frontend/src/hooks/useNotes.js
@@ -0,0 +1,29 @@
+/** @format */
+import { useEffect, useState } from "react";
+import { getAllNotesService } from "../services";
+
+const useNotes = () => {
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const loadNotes = async () => {
+      try {
+        setLoading(true);
+
+        const data = await getAllNotesService();
+        setNotes(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    loadNotes();
+  }, []);
+
+  return { notes, loading, error };
+};
+
+export default useNotes;
