d35e72883eb4fa54dea48a103fca3bc83baed6e0
JorGBit <113969001+JorGBit@users.noreply.github.com>
2023-03-05 19:57:40 +0100

    actualiced

--- notes-frontend/src/components/NoteList.js
+++ notes-frontend/src/components/NoteList.js
@@ -1,40 +1,23 @@
 /** @format */
 
-// /** @format */
-// import { Note } from "./Note";
-// export const NoteList = ({ notes, removeNote }) => {
-//   console.log(notes);
-//   return notes && notes.length ? (
-//     <ul>
-//       {notes.map((note) => (
-//         <li key={notes.id}>
-//           <Note note={note} removeNote={removeNote} />
-//         </li>
-//       ))}
-//     </ul>
-//   ) : (
-//     <p>There are no notes yet...</p>
-//   );
-// };
-
-import { Link } from 'react-router-dom';
-import { Note } from './Note';
+import { Link } from "react-router-dom";
+import { Note } from "./Note";
 
 export const NoteList = ({ notes, removeNote }) => {
-    console.log(notes);
-    return notes && notes.length ? (
-        <ul>
-            {notes.map((note) => (
-                <li key={note.id}>
-                    <h2>{note.tittle}</h2>
+  console.log(notes);
+  return notes && notes.length ? (
+    <ul>
+      {notes.map((note) => (
+        <li key={note.id}>
+          <h2>{note.tittle}</h2>
 
-                    <Link to={`/noteInfo/${note.id}`}>
-                        <button className="btn_openNot">Open note</button>
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    ) : (
-        <p>There are no notes yet...</p>
-    );
+          <Link to={`/noteInfo/${note.id}`}>
+            <button>Open note</button>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <p>There are no notes yet...</p>
+  );
 };
