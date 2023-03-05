1eb40630f7c0a086772d3159de3681eb457155be
JorGBit <113969001+JorGBit@users.noreply.github.com>
2023-03-05 19:06:01 +0100

    actualiced

--- notes-frontend/src/components/NoteList.js
+++ notes-frontend/src/components/NoteList.js
@@ -1,22 +1,5 @@
 /** @format */
 
-// /** @format */
-// import { Note } from "./Note";
-// export const NoteList = ({ notes, removeNote }) => {
-//   console.log(notes);
-//   return notes && notes.length ? (
-//     <ul>
-//       {notes.map((note) => (
-//         <li key={notes.id}>
-//           <Note note={note} removeNote={removeNote} />
-//         </li>
-//       ))}
-//     </ul>
-//   ) : (
-//     <p>There are no notes yet...</p>
-//   );
-// };
-
 import { Link } from 'react-router-dom';
 import { Note } from './Note';
 
