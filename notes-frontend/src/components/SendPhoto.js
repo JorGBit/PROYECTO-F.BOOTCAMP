da1383e020a2d231199c235ffa239c87d4945ec4
JorGBit <113969001+JorGBit@users.noreply.github.com>
2023-03-05 19:58:36 +0100

    actualiced

--- notes-frontend/src/components/SendPhoto.js
+++ notes-frontend/src/components/SendPhoto.js
@@ -1,45 +1,59 @@
 /** @format */
 
-import { sendPhotoService } from "../services/index";
-import { AuthContext } from "../context/AuthContext";
 import { useContext, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { uploadPhotoService } from "../services/index";
 
-export const AddImageForm = ({ noteId }) => {
-  const [image, setImage] = useState(null);
-  const [error, setError] = useState(null);
+export const SendPhoto = ({ noteId }) => {
+  const [error, setError] = useState("");
   const [sending, setSending] = useState(false);
   const { token } = useContext(AuthContext);
+  const [image, setImage] = useState();
 
-  const handleImageChange = (e) => {
+  const handleForm = async (e) => {
     e.preventDefault();
-    setImage(e.target.files[0]);
-  };
 
-  const handleFormSubmit = async (e) => {
     try {
       setSending(true);
 
-      const data = new FormData(e.target);
-      const note = await sendPhotoService({ data, token });
-      noteId(note);
-      const formData = new FormData();
+      const data = new FormData();
+      data.append("image", image);
 
-      formData.append("photo", image);
-      const result = await sendPhotoService({ data: formData, noteId });
-      console.log(result);
+      await uploadPhotoService({ data, noteId, token });
+      e.target.reset();
+      setImage(null);
     } catch (error) {
+      setError(error.message);
+    } finally {
       setSending(false);
     }
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
-      <h3>Add Image</h3>
-      <input type="file" onChange={handleImageChange} />
-      <button type="submit">Submit</button>
-      {error && <div>{error}</div>}
+    <form onSubmit={handleForm}>
+      <fieldset>
+        <label htmlFor="image">Image</label>
+        <input
+          type="file"
+          id="image"
+          name="image"
+          accept="image/*"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+
+        {image ? (
+          <figure>
+            <img
+              src={URL.createObjectURL(image)}
+              alt="Preview"
+              style={{ width: "100px" }}
+            />
+          </figure>
+        ) : null}
+      </fieldset>
+      <button>Send Image</button>
+      {sending ? <p>Sending Image</p> : null}
+      {error ? <p>{error}</p> : null}
     </form>
   );
 };
-
-export default AddImageForm;
