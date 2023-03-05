4ec8d3746b8a38d4f44ef19982fb4e6eadd5d480
JorGBit <113969001+JorGBit@users.noreply.github.com>
2023-03-05 19:21:50 +0100

    actualiced

--- notes-frontend/src/App.js
+++ notes-frontend/src/App.js
@@ -1,40 +1,40 @@
 /** @format */
 
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { Footer } from './components/Footer';
-import { Header } from './components/Header';
-import { HomePage } from './pages/HomePage';
-import { RegisterPage } from './pages/RegisterPage';
-import { LoginPage } from './pages/LoginPage';
-import { NotePage } from './pages/NotePage';
-import { NotFoundPage } from './pages/NotFoundPage';
-import { NoteListPage } from './pages/NoteListPage';
-import { UserPage } from './pages/UserPage';
-import { NewNotePage } from './pages/NewNotePage';
-import { EditNotePage } from './pages/EditNotePage';
-import AddPhotoPage from './pages/AddPhotoPage';
-
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
+import { HomePage } from "./pages/HomePage";
+import { RegisterPage } from "./pages/RegisterPage";
+import { LoginPage } from "./pages/LoginPage";
+import { NotePage } from "./pages/NotePage";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import { NoteListPage } from "./pages/NoteListPage";
+import { UserPage } from "./pages/UserPage";
+import { NewNotePage } from "./pages/NewNotePage";
+import { EditNotePage } from "./pages/EditNotePage";
+import { AddPhotoPage } from "./pages/AddPhotoPage";
 function App() {
-    return (
-        <main>
-            <Header />
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/newNote" element={<NewNotePage />} />
-                <Route path="/noteInfo/:id" element={<NotePage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/users" element={<UserPage />} />
-                <Route path="/noteList" element={<NoteListPage />} />
-                <Route path="/notes/:id/photo" element={<AddPhotoPage />} />
-                <Route path="/notesEdit/:id" element={<EditNotePage />} />
-                <Route path="*" element={<NotFoundPage />} />
-            </Routes>
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/newNote" element={<NewNotePage />} />
+        <Route path="/noteInfo/:id" element={<NotePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/users" element={<UserPage />} />
+        <Route path="/noteList" element={<NoteListPage />} />
+        <Route path="/notes/:id/photo" element={<AddPhotoPage />} />
+        <Route path="/notesEdit/:id" element={<EditNotePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
 
-            <Footer />
-        </main>
-    );
+      <Footer />
+    </main>
+  );
 }
 
 export default App;
+
